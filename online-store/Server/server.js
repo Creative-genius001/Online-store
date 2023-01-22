d573a63cc59eef6dec4e06688da8157628ee68c8
Creative-genius001 <john.henry5923@gmail.com>
2023-01-21 16:48:53 -1200

    update

---
+++ online-store/Server/server.js
@@ -0,0 +1,55 @@
+const express = require("express");
+require("dotenv").config();
+const stripe = require("stripe")(
+	process.env.SECRET_KEY,
+);
+const app = express();
+const cors = require("cors");
+app.use(express.json());
+app.use(
+	cors({
+		origin: "http://localhost:3000",
+	}),
+);
+
+const YOUR_DOMAIN = "http://localhost:3000";
+
+const products = [
+	{ name: "shoe", quantity: 1, price: 20 },
+	{ name: "clothes", quantity: 5, price: 25 },
+];
+
+app.post(
+	"/create-checkout-session",
+	async (req, res) => {
+		try {
+			const session =
+				await stripe.checkout.sessions.create({
+					payment_method_types: ["card"],
+					line_items: products.map((product) => {
+						return {
+							price_data: {
+								currency: "usd",
+								product_data: {
+									name: product.name,
+								},
+								unit_amount: product.price * 100,
+							},
+							quantity: product.quantity,
+						};
+					}),
+					mode: "payment",
+					success_url: `${YOUR_DOMAIN}?success=true`,
+					cancel_url: `${YOUR_DOMAIN}?canceled=true`,
+				});
+
+			res.json({ url: session.url });
+		} catch (e) {
+			res.status(500).json({ error: e.message });
+		}
+	},
+);
+
+app.listen(5500, () => {
+	console.log(`running on port ${5500}`);
+});
---
+++ online-store/client/src/contextApi/commerceAPI.js
@@ -0,0 +1,150 @@
+import {
+	createContext,
+	useContext,
+	useState,
+} from "react";
+import Commerce from "@chec/commerce.js";
+
+export const CommmerceContext = createContext();
+
+export function CommerceContextProvider({
+	children,
+}) {
+	const commerce = new Commerce(
+		process.env.REACT_APP_PUBLIC_KEY,
+	);
+	const [products, setProducts] = useState([]);
+	const [product, setProduct] = useState([]);
+	const [cartList, setCartList] = useState([]);
+	const [cartLength, setCartLength] = useState();
+	const [allCategories, setAllCategories] =
+		useState([]);
+	const [subTotal, setSubTotal] = useState();
+	const [numberInCart, setNumberInCart] =
+		useState(0);
+
+	const getCartNumber = async () => {
+		await commerce.cart
+			.retrieve()
+			.then((cart) => {
+				setNumberInCart(cart.total_items);
+			});
+	};
+
+	const getProducts = async () => {
+		await commerce.products
+			.list()
+			.then((product) => {
+				setProducts(product.data);
+			});
+	};
+	const getProduct = async (id) => {
+		await commerce.products
+			.retrieve(id)
+			.then((res) => {
+				setProduct([res]);
+			});
+	};
+
+	const addToCart = async (id) => {
+		commerce.cart.add(id, 1).then((response) => {
+			setNumberInCart(response.total_items);
+		});
+	};
+
+	const getCartContents = async () => {
+		// await commerce.cart
+		// 	.contents()
+		// 	.then((items) => setCartList(items));
+		await commerce.cart
+			.retrieve()
+			.then((cart) => {
+				setCartList(cart.line_items);
+				setSubTotal(cart.subtotal.raw);
+				setCartLength(cart.total_items);
+			});
+	};
+
+	const deleteItemFromCart = async (id) => {
+		await commerce.cart
+			.remove(id)
+			.then((response) => {
+				setCartList(response.line_items);
+				setSubTotal(response.subtotal.raw);
+			});
+	};
+
+	const getAllCategories = async () => {
+		await commerce.categories
+			.list()
+			.then((category) =>
+				setAllCategories(category.data),
+			);
+	};
+
+	const getSingleCat = async (id) => {
+		await commerce.categories
+			.retrieve(id)
+			.then((category) => {
+				console.log(category.slug);
+				let catName = category.slug;
+				let catDetails = products.filter(
+					(product) => {
+						return (
+							product.categories[0].slug ==
+							catName
+						);
+					},
+				);
+				console.log(catDetails);
+				setProducts(catDetails);
+			});
+	};
+
+	async function updateCartInc(id, quantity) {
+		quantity = quantity + 1;
+		await commerce.cart
+			.update(id, {
+				quantity,
+			})
+			.then((response) =>
+				setSubTotal(response.subtotal.raw),
+			);
+	}
+
+	async function updateCartDec(id, quantity) {
+		quantity = quantity - 1;
+		await commerce.cart
+			.update(id, {
+				quantity: quantity--,
+			})
+			.then((response) =>
+				setSubTotal(response.subtotal.raw),
+			);
+	}
+
+	return (
+		<CommmerceContext.Provider
+			value={{
+				updateCartInc,
+				updateCartDec,
+				cartLength,
+				getCartNumber,
+				getAllCategories,
+				allCategories,
+				getSingleCat,
+				deleteItemFromCart,
+				cartList,
+				subTotal,
+				getCartContents,
+				getProducts,
+				addToCart,
+				getProduct,
+				products,
+				product,
+				numberInCart,
+			}}>
+			{children}
+		</CommmerceContext.Provider>
+	);
+}
